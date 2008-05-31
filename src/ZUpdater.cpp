985328bb9f160f8c3d1d279be68be146b99b46e1
qbasicer <qbasicer@gmail.com>
2008-05-31 15:43:05 +0000

    Added Mac and Windows versioning support for ZUpdater

--- src/ZUpdater.cpp
+++ src/ZUpdater.cpp
@@ -61,8 +61,69 @@ void ZUpdater::fetch(){
 		ua += ZDL_VERSION_STRING;
 		ua += " (";
 #ifdef Q_WS_WIN
+		if (QSysInfo::WindowsVersion & QSysInfo::WV_DOS_based){
+			ua += "Win32; ";
+			if (QSysInfo::WindowsVersion == QSysInfo::WV_32s){
+				ua += "Windows 3.1";
+			}else if (QSysInfo::WindowsVersion == QSysInfo::WV_95){
+				ua += "Windows 95";
+			}else if (QSysInfo::WindowsVersion == QSysInfo::WV_98){
+				ua += "Windows 98";
+			}else if (QSysInfo::WindowsVersion == QSysInfo::WV_Me){
+				ua += "Windows ME";
+			}else {
+				ua += "Windows Unknown";
+			}
+		}else if (QSysInfo::WindowsVersion & QSysInfo::WV_NT_based){
+			ua += "WinNT; ";
+			if (QSysInfo::WindowsVersion == QSysInfo::WV_NT){
+				ua += "Windows NT";
+			}else if (QSysInfo::WindowsVersion == QSysInfo::WV_2000){
+				ua += "Windows 2000";
+			}else if (QSysInfo::WindowsVersion == QSysInfo::WV_XP){
+				ua += "Windows XP";
+			}else if (QSysInfo::WindowsVersion == QSysInfo::WV_2003){
+				ua += "Windows Server 2003";
+			}else if (QSysInfo::WindowsVersion == QSysInfo::WV_VISTA){
+				ua += "Windows Vista";
+			}else{
+				ua += "Windows Unknown";
+			}
+				
+		}else if (QSysInfo::WindowsVersion & QSysInfo::WV_CE_based){
+			if (QSysInfo::WindowsVersion == QSysInfo::WV_CE){
+				ua += "Windows CE";
+			}else if (QSysInfo::WindowsVersion == QSysInfo::WV_CENET){
+				ua += "Windows CS .NET";
+			}else{
+				ua += "Windows Unknown";
+			}
+			
+			ua += "WinCE; ";
+		}else{
+			ua += "WinUnknown";
+		}
 #else
 #ifdef Q_WS_MAC 
+		if (QSysInfo::MacintoshVersion == QSysInfo::MV_9){
+			ua += "MacOS9";
+		}else if (QSysInfo::MacintoshVersion == QSysInfo::MV_9){
+			ua += "MacOSX 10.0 Cheetah";
+		}else if (QSysInfo::MacintoshVersion == QSysInfo::MV_9){
+			ua += "MacOSX 10.1 Puma";
+		}else if (QSysInfo::MacintoshVersion == QSysInfo::MV_9){
+			ua += "MacOSX 10.2 Jaguar";
+		}else if (QSysInfo::MacintoshVersion == QSysInfo::MV_9){
+			ua += "MacOSX 10.3 Panther";
+		}else if (QSysInfo::MacintoshVersion == QSysInfo::MV_9){
+			ua += "MacOSX 10.4 Tiger";
+		}else if (QSysInfo::MacintoshVersion == QSysInfo::MV_9){
+			ua += "MacOSX 10.5 Leopard";
+		}else if (QSysInfo::MacintoshVersion == QSysInfo::MV_Unknown){
+			ua += "Mac Unknown";
+		}else{
+			ua += "Mac Unknown";
+		}
 #else
 			ua += "Linux ";
 			QFile kernel("/proc/sys/kernel/osrelease");
