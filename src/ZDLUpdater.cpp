5b36c428a03af8a16b10621d9efc8881d4f77e68
Cody Harris <qbasicer@gmail.com>
2012-04-04 15:14:50 -0400

    Fix user agent for update checks

--- src/ZDLUpdater.cpp
+++ src/ZDLUpdater.cpp
@@ -101,7 +101,7 @@ void ZDLUpdater::fetch(int doAnyways){
 #endif
 		url += QString::number(ZDL_VERSION_ID);
 		QHttpRequestHeader qreq("GET", url);
-		QString ua = versionString;
+		QString ua = QString(ZDL_ENGINE_NAME) + QString(" ") + versionString;
 		ua += " (";
 #ifdef Q_WS_WIN
 		if (QSysInfo::WindowsVersion & QSysInfo::WV_DOS_based){
