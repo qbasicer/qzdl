feaa4eb58380e4fb1c1991b6c69c42ba8e44d758
Cody Harris <qbasicer@gmail.com>
2012-04-05 15:03:55 -0400

    Clean up the dialog box a little

--- src/ZDLAboutDialog.cpp
+++ src/ZDLAboutDialog.cpp
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
- 
+
 #include "ZDLAboutDialog.h"
 #include "ZDLConfigurationManager.h"
 #include <QDialogButtonBox>
@@ -30,62 +30,70 @@ ZDLAboutDialog::ZDLAboutDialog(ZQWidget *parent):QDialog(parent){
 	QHBoxLayout *hbox = new QHBoxLayout();
 	box->addLayout(hbox);
 	QVBoxLayout *vbox = new QVBoxLayout();
-	QLabel *title = new QLabel("ZDL<i>Sharp</i> "+versionString,this);
-	
+	QLabel *title = new QLabel(QString("ZDL<i>Sharp</i> ")+ZDL_VERSION_STRING,this);
+
 	QFont font;
 	font.setPointSize(24);
 	title->setFont(font);
 	title->setAlignment(Qt::AlignHCenter);
-	
+
 	vbox->addWidget(title);
-	vbox->addWidget(new QLabel(QString("Build: ")+QString(ZDL_BUILD),this));
-	vbox->addWidget(new QLabel(QString("Revision: ")+QString(ZDL_REVISION),this));
+
+	QLabel *pic = new QLabel(this);
+	pic->setPixmap(QPixmap(aboutImg));
+
+	hbox->addWidget(pic);
+	hbox->addLayout(vbox);
+	QFrame *hrTop = new QFrame(this);
+	hrTop->setFrameStyle(QFrame::HLine);
+	box->addWidget(hrTop);
+	box->addWidget(new QLabel(QString("Source: ")+QString(ZDL_SOURCE),this));
+	box->addWidget(new QLabel("C Version Copyright (C) BioHazard 2005",this));
+	box->addWidget(new QLabel("C# Version Copyright (C) QBasicer 2007",this));
+	box->addWidget(new QLabel("Qt Version Copyright (C) Cody Harris 2007-2012",this));
+	QLabel *url = new QLabel("<a href=http://zdl.vectec.net>http://zdl.vectec.net</a>",this);
+	url->setOpenExternalLinks(true);
+	box->addWidget(url);
+	box->addWidget(new QLabel(QString("Source: ")+QString(ZDL_SOURCE),this));	
+	box->addWidget(new QLabel(QString("Build: ")+QString(ZDL_BUILD),this));
+	box->addWidget(new QLabel(QString("Revision: ")+QString(ZDL_REVISION),this));
 #if defined(ZDL_BUILD_NUMBER)
 	if(ZDL_BUILD_NUMBER > 0){
-		vbox->addWidget(new QLabel(QString("Build #: ")+QString::number(ZDL_BUILD_NUMBER),this));
+		box->addWidget(new QLabel(QString("Build #: ")+QString::number(ZDL_BUILD_NUMBER),this));
 	}
 #endif
 #if defined(ZDL_BUILD_JOB)
-	vbox->addWidget(new QLabel(QString("Build job: ")+QString(ZDL_BUILD_JOB),this));
+	box->addWidget(new QLabel(QString("Build job: ")+QString(ZDL_BUILD_JOB),this));
 #endif
-	vbox->addWidget(new QLabel(QString("Source: ")+QString(ZDL_SOURCE),this));
-	vbox->addWidget(new QLabel("C Version Copyright (C) BioHazard 2005",this));
-	vbox->addWidget(new QLabel("C# Version Copyright (C) QBasicer 2007",this));
-	vbox->addWidget(new QLabel("Qt Version Copyright (C) Cody Harris 2007-2011",this));
-	QLabel *url = new QLabel("<a href=http://zdl.vectec.net>http://zdl.vectec.net</a>",this);
-	url->setOpenExternalLinks(true);
-	vbox->addWidget(url);
-	
-	QLabel *pic = new QLabel(this);
-	pic->setPixmap(QPixmap(aboutImg));
-	
-	hbox->addWidget(pic);
-	hbox->addLayout(vbox);
-	
-	
+	QFrame *hrMid = new QFrame(this);
+	hrMid->setFrameStyle(QFrame::HLine);
+	box->addWidget(hrMid);
+
 	box->addWidget(new QLabel("Special thanks to BioHazard for the original version",this));
 	box->addWidget(new QLabel("Huge thanks to NeuralStunner.  Without his help, none of this would be possible.", this));
 	box->addWidget(new QLabel("Special thanks to Risen, Enjay, DRDTeam.org, ZDoom.org",this));
-	
+
 	ZDLConfiguration *conf = ZDLConfigurationManager::getConfiguration();
 	if(conf){
 		QString systemConfPath = conf->getPath(ZDLConfiguration::CONF_SYSTEM);
 		QString userConfPath = conf->getPath(ZDLConfiguration::CONF_USER);
-		
-		box->addWidget(new QLabel(" ",this));
-		
-		QLabel *systemConf = new QLabel("System Configuration File: "+systemConfPath,this);
-		systemConf->setTextInteractionFlags(Qt::TextSelectableByMouse);
-		
+
+		QFrame *hrBot = new QFrame(this);
+		hrBot->setFrameStyle(QFrame::HLine);
+		box->addWidget(hrBot);
+
+		/*QLabel *systemConf = new QLabel("System Configuration File: "+systemConfPath,this);
+		  systemConf->setTextInteractionFlags(Qt::TextSelectableByMouse);
+		  box->addWidget(systemConf);*/
+
 		QLabel *userConf = new QLabel("User Configuration File: "+userConfPath,this);
 		userConf->setTextInteractionFlags(Qt::TextSelectableByMouse);
-		
-		box->addWidget(systemConf);
+
 		box->addWidget(userConf);
 	}
-	
-	
-	
+
+
+
 	QDialogButtonBox *btnBox = new QDialogButtonBox(QDialogButtonBox::Ok,Qt::Horizontal,this);
 	box->addWidget(btnBox);
 	connect(btnBox, SIGNAL(accepted()), this, SLOT(close()));
