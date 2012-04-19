88e20cd4482fb2982cfee3d4152600c3b05badeb
Cody Harris <qbasicer@gmail.com>
2012-04-19 16:51:08 -0400

    Allow users to import the current loaded config and thusly replace the current global config.

--- include/ZDLInterface.h
+++ include/ZDLInterface.h
@@ -50,6 +50,7 @@ private slots:
 	void clearAllFields();
 	void clearAllPWads();
 	void clearEverything();
+	void importCurrentConfig();
 private:
 	QLayout *getBottomPane();
 	QLayout *getButtonPane();
