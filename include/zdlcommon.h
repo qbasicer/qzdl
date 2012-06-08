f0c2086914f2cdae24e9087f9cdd8b65b0074da6
Cody Harris <qbasicer@gmail.com>
2012-06-08 12:23:12 -0400

    Bump version numbers and commonized the product name between Windows and Non-Windows

--- include/zdlcommon.h
+++ include/zdlcommon.h
@@ -22,18 +22,14 @@
 using namespace std;
 #define ZDL_FLAG_NAMELESS	0x00001
 
-#ifdef Q_WS_WIN
+#define ZDL_VERSION_STRING	"3.2.0.0 beta"
 
-#define ZDL_VERSION_STRING	"3.0.5.9"
+#ifdef Q_WS_WIN
 #define ZDL_VERSION_ID		33
 #define ZDL_PRODUCT_ID		"qzdl-win32-stable"
-#define ZDL_STABLE
 #else
-
-#define ZDL_VERSION_STRING	"3.0.5.9"
 #define ZDL_VERSION_ID		34
 #define ZDL_PRODUCT_ID		"qzdl-git"
-
 #endif
 
 #define ZDL_UDPATE_SERVER	"update.vectec.net"
