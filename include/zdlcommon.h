8da133d0d8afe5801e7c5fb7e933f66f2bfff89e
Cody Harris <qbasicer@gmail.com>
2012-06-27 16:58:22 -0400

    Bump internal version number and update number

--- include/zdlcommon.h
+++ include/zdlcommon.h
@@ -29,13 +29,13 @@ using namespace std;
 
 #ifdef Q_WS_WIN
 
-#define ZDL_VERSION_STRING	"3.0.5.10"
-#define ZDL_VERSION_ID		36
+#define ZDL_VERSION_STRING	"3.0.5.11"
+#define ZDL_VERSION_ID		37
 #define ZDL_PRODUCT_ID		"qzdl-win32-stable"
 #define ZDL_STABLE
 #else
 
-#define ZDL_VERSION_STRING	"3.0.5.10"
+#define ZDL_VERSION_STRING	"3.0.5.11"
 #define ZDL_VERSION_ID		34
 #define ZDL_PRODUCT_ID		"qzdl-git"
 
