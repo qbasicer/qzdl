93cca330ffd35b8474a4a201d09db65900354ba2
Kevin Caccamo <kevin@ciinet.org>
2016-12-17 03:17:35 -0500

    Import Qt GUI classes in header files.

--- include/ZDLSettingsTab.h
+++ include/ZDLSettingsTab.h
@@ -15,9 +15,10 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
- 
-#include <QtGui>
+
 #include <QObject>
+#include <QCheckBox>
+#include <QLineEdit>
 #include "ZDLWidget.h"
 #include "ZDLSourcePortList.h"
 #include "ZDLIWadList.h"
