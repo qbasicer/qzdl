8530bfbb6739f5a6e82329fbf8b116d92464ecca
Kevin Caccamo <kevin@ciinet.org>
2016-12-17 03:40:14 -0500

    Fix compilation of ZDLFileList and ZDLFilePane.

--- src/ZDLFilePane.cpp
+++ src/ZDLFilePane.cpp
@@ -16,9 +16,11 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
  
-#include <QtGui>
+#include <QWidget>
 #include <QApplication>
 #include <QListWidget>
+#include <QVBoxLayout>
+#include <QLabel>
 #include <iostream>
 
 #include "ZDLListWidget.h"
