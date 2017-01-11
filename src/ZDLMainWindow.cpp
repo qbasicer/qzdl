5318f9293d888a71dd1c95ed5786678ad23f97d4
Dugan Chen <thedoogster@gmail.com>
2017-01-11 06:38:36 -0800

    Just start the QProcess detached. No reason not to.

--- src/ZDLMainWindow.cpp
+++ src/ZDLMainWindow.cpp
@@ -340,13 +340,7 @@ void ZDLMainWindow::launch(){
 	proc->setWorkingDirectory(workingDirectory);
 
 	proc->setProcessChannelMode(QProcess::ForwardedChannels);
-
-	if (zconf->hasValue("zdl.general", "autoclose")) {
-		proc->startDetached(exec, args);
-	} else {
-		proc->start(exec, args);
-	}
-
+	proc->startDetached(exec, args);
 	procerr = proc->error();
 #endif
 	int stat;
