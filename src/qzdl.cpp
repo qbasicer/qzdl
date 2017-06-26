658d4fe84c6d00ba3972841974349a04f1634d66
Dugan Chen <thedoogster@gmail.com>
2017-06-25 22:39:12 -0700

    Fix: 'QIODevice::write (QIODevice): device not open'

--- src/qzdl.cpp
+++ src/qzdl.cpp
@@ -74,6 +74,7 @@ int main( int argc, char **argv ){
 	}
 	QStringList eatenArgs(args);
 	ZDLNullDevice nullDev;
+	nullDev.open(QIODevice::WriteOnly);
 #if defined(ZDL_BLACKBOX)
 	QFile *loggingFile = NULL;
 	zdlDebug = NULL;
@@ -305,6 +306,7 @@ int main( int argc, char **argv ){
 
 	tconf->writeINI(ZDLConfigurationManager::getConfigFileName());
 	LOGDATA() << "ZDL QUIT" << endl;
+	nullDev.close();
 	return ret;
 }
 
