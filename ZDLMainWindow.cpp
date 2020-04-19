4947aa2bb395b2c62b0af717715c0bbe83dcc846
Dugan Chen <thedoogster@gmail.com>
2020-04-19 14:52:59 -0700

    Revert "Use lcferrum's solution for extra-args and always-add"
    This reverts commit ddbc0b2138f4ab606b36a4a7d9201e91746c0fda.

--- ZDLMainWindow.cpp
+++ ZDLMainWindow.cpp
@@ -27,7 +27,7 @@
 #include "zdlcommon.h"
 #include "ZDLInfoBar.h"
 #include "ico_icon.xpm"
-#include <wordexp.h>
+#include <QDebug>
 
 extern QApplication *qapp;
 extern QString versionString;
@@ -222,23 +222,6 @@ void ZDLMainWindow::badLaunch(){
 	}
 }
 
-QStringList ParseParams(const QString& params)
-{
-	QStringList plist;
-
-	wordexp_t result;
-
-	switch (wordexp(qPrintable(params), &result, 0)) {
-		case 0:
-			for (size_t i=0; i<result.we_wordc; i++)
-				plist<<result.we_wordv[i];
-		case WRDE_NOSPACE:	//If error is WRDE_NOSPACE - there is a possibilty that at least some part of wordexp_t.we_wordv was allocated
-			wordfree (&result);
-	}
-
-	return plist;
-}
-
 QStringList ZDLMainWindow::getArguments(){
 	QStringList ourString;
 	auto zconf = ZDLConfigurationManager::getActiveConfiguration();
@@ -393,11 +376,15 @@ QStringList ZDLMainWindow::getArguments(){
 	}
 
 	if (zconf->contains("zdl.general/alwaysadd")){
-		ourString << ParseParams(zconf->value("zdl.save/extra").toString());
+		for (auto arg: zconf->value("zdl.save/extra").toString().split(QRegExp("\\s+"), QString::SkipEmptyParts)) {
+			ourString << arg;
+		}
 	}
 
 	if (zconf->contains("zdl.save/extra")){
-		ourString << ParseParams(zconf->value("zdl.save/extra").toString());
+		for (auto arg: zconf->value("zdl.save/extra").toString().split(QRegExp("\\s+"), QString::SkipEmptyParts)) {
+			ourString << arg;
+		}
 	}
 
 	return ourString;
